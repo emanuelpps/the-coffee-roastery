c2aab4b2a59cb1549b7e654fc6e5cb7447c0c05c
emanuelpps <emanuelpages.ps@gmail.com>
2024-06-13 14:15:00 -0300

    styles corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Router from "./routes/Router";
 
 function App() {
-  return <Router className="font-playfair"/>;
+  return <Router className="font-playfair max-w-[1200px] mx-auto"/>;
 }
 
 export default App;
--- src/pages/Home/components/intro/Intro.jsx
+++ src/pages/Home/components/intro/Intro.jsx
@@ -4,11 +4,13 @@ function Intro() {
   return (
     <div
       id="intro"
-      className={`px-8 pt-[380px] md:px-20 md:pt-[280px] text-[#ffebd6] pb-20 h-screen w-full  md:bg-up-left bg-cover bg-no-repeat overflow-y-hidden bg-background-intro-mobile md:bg-background-intro`}
+      className={`px-8 pt-[380px] md:px-[5%] md:pt-[20%] text-[#ffebd6] pb-20 bg-cover min-h-screen w-full  md:bg-up-left  bg-no-repeat overflow-y-hidden bg-background-intro-mobile md:bg-background-intro`}
     >
-      <h1 className="font-[500] text-[3rem]  font-playfair md:w-[45%] md:text-[75px] leading-none">
-        From Our Roastery to Your Cup, Pure Perfection
-      </h1>
+      <div className="flex item-center ">
+        <h1 className="font-[500] text-[3rem]  font-playfair md:w-[45%] md:text-[75px] leading-none">
+          From Our Roastery to Your Cup, Pure Perfection
+        </h1>
+      </div>
       <div className="mt-5">
         <Buttons variant="primary" label="Learn More"></Buttons>
       </div>
