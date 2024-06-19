3cdd6ffa02505d73351beea422d41a17afba57c5
emanuelpps <emanuelpages.ps@gmail.com>
2024-06-19 13:58:10 -0300

    added cart widget hover

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { PiShoppingCartDuotone } from "react-icons/pi";
 import { Link } from "react-router-dom";
 import useCartStore from "../../store/CartStore";
 
 function CartWidget() {
   const { cart } = useCartStore();
+  const [hoverState, setHoverState] = useState(false);
 
   return (
     <div
@@ -14,15 +15,21 @@ function CartWidget() {
       <Link
         to="/cart"
         id="cart-widget-container"
-        className="bg-[#473429] rounded-full p-5  fixed bottom-5 right-5"
+        className="bg-[#473429]  hover:bg-[#FFEBD6]  rounded-full p-5  fixed bottom-5 right-5"
+        onMouseEnter={() => setHoverState(true)}
+        onMouseLeave={() => setHoverState(false)}
       >
         <PiShoppingCartDuotone
           id="cart-widget-icon"
-          className="text-[#FFEBD6] text-[20px]"
+          className={`${
+            hoverState ? "text-[#473429]" : "text-[#FFEBD6]"
+          } text-[20px]`}
         />
         <div
           id="cart-widget-quantity"
-          className="absolute top-1 right-3 rounded-full text-[#FFEBD6] w-auto"
+          className={`absolute top-1 right-3 rounded-full ${
+            hoverState ? "text-[#473429]" : "text-[#FFEBD6]"
+          }  w-auto`}
         >
           <span className="w-full top-1 right-3">{cart.length}</span>
         </div>
